b8156af0888c6442264f18398f4e6ed09ede0693
mahtabza <mahdiyehzamani546@gmail.com>
2021-09-27 23:59:08 +0330

    main menu added

---
+++ mainmenu.py
@@ -0,0 +1,49 @@
+import sys
+import logging
+from registration import Register
+from login import LoginToAccount
+
+
+class MainMenu:
+    """Display a menu and respond to choices when run."""
+
+    def __init__(self):
+        self.choices = {
+            "1": self.registration,
+            "2": self.login_to_account,
+            "3": self.quit
+        }
+
+    @staticmethod
+    def display_menu():
+        print("""
+*Menu*
+
+1. Registration
+2. Login to account
+3. Quit
+        """)
+
+    def run_main(self):
+        """Display the menu and respond to choices."""
+        while True:
+            self.display_menu()
+            choice = input("Enter an option: ")
+            action = self.choices.get(choice)
+            if action:
+                action()
+            else:
+                print(f'{choice}is not a valid choice!')
+                logging.warning("invalid choice")
+
+    def registration(self):
+        Register()
+
+    def login_to_account(self):
+        LoginToAccount()
+
+    def quit(self):
+        sys.exit(0)
+
+
+MainMenu().run_main()
